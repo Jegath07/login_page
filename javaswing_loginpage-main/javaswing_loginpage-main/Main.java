38e0b8c79f3278823e0e6cdb283c761b595b46ac
Jegath07 <jegathsr6@gmail.com>
2025-08-28 10:29:08 +0530

    Delete javaswing_loginpage-main/javaswing_loginpage-main directory

--- javaswing_loginpage-main/javaswing_loginpage-main/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args){
-        IDandPasswords idandpasswords=new  IDandPasswords();
-        loginpage login=new loginpage(idandpasswords.logininfo);
-    }
-}
